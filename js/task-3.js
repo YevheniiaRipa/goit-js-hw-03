8d05d03e8aaa536f3a899eb711a84089adf15ca6
Yevhiniia Ripa <evgeniarepa@gmail.com>
2025-07-15 17:51:01 +0300

    Add HW3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = Number.parseFloat(content);
-  const paddingValue = Number.parseFloat(padding);
-  const borderValue = Number.parseFloat(border);
-
-  return contentValue + 2 * paddingValue + 2 * borderValue;
+function filterArray(numbers, value) {
+  const suitableNumbers = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      suitableNumbers.push(numbers[i]);
+    }
+  }
+  return suitableNumbers;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
